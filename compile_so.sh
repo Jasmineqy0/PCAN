4c9b602938775b41e6dfb5a9245d7fc1dd2fdff9
jasmineqy0 <jasmineqy0@gmail.com>
2022-04-18 17:22:05 +0200

    revised compile script

--- compile_so.sh
+++ compile_so.sh
@@ -1,11 +1,11 @@
-apt install gcc-5 g++-5
+apt-get install gcc-5 g++-5
 ln -s /usr/bin/gcc-5 /usr/local/cuda/bin/gcc
 ln -s /usr/bin/g++-5 /usr/local/cuda/bin/g++
 
 TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
 TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
-cd tf_ops/sampling/
+cd /home/PCAN/tf_ops/sampling/
 
 nvcc tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -Wno-deprecated-gpu-targets
 
@@ -17,14 +17,14 @@ g++ -w -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -share
     -lcudart -L $CUDA_PREDIX"/lib64/" -L $TF_PREFIX \
     -O2 -D_GLIBCXX_USE_CXX11_ABI=0 -I$TF_INC/external/nsync/public -L$TF_LIB -ltensorflow_framework
 
-cd tf_ops/3d_interpolation/
+cd /home/PCAN/tf_ops/3d_interpolation/
 
 g++ -w -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC \ 
     -I $TF_PREFIX"/include" -I $CUDA_PREDIX"/include" -I $TF_PREFIX"/include/external/nsync/public" \
     -lcudart -L $CUDA_PREDIX"/lib64/" -L $TF_PREFIX \
     -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0 -I$TF_INC/external/nsync/public -L$TF_LIB -ltensorflow_framework
 
-cd tf_ops/grouping/
+cd /home/PCAN/tf_ops/grouping/
 
 g++ -w -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC \
     -I $TF_PREFIX"/include" -I $CUDA_PREDIX"/include" -I $TF_PREFIX"/include/external/nsync/public" \
